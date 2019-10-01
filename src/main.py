cf8f855eb99da215daf8f441f74b142cf7a6ffb4
Antoine <antoine.sb@orange.fr>
2019-10-01 11:43:36 +0200

    :pencil: add documentation

--- src/main.py
+++ src/main.py
@@ -1,6 +1,20 @@
 # -*- coding: utf-8 -*-
 
-"""This module generates aims to solve task scheduling problems."""
+"""This module aims to solve task scheduling problems."""
+
+"""
+Resources
+- https://algorithm-visualizer.org/greedy/job-scheduling-problem
+- https://visualgo.net/en/graphds
+
+Data structure : acyclic directed weighted graph
+
+Analysis
+- https://github.com/nedbat/coveragepy
+- https://github.com/facebook/pyre-check
+- https://github.com/python/mypy
+- https://gitlab.com/pycqa/flake8
+"""
 
 def main():
 	"""Script entry point"""
