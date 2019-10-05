a5a5810df4c2138f9185146eca0f8395ced349f4
Antoine <antoine.sb@orange.fr>
2019-10-05 08:40:27 +0200

    :white_check_mark: move tests in tests.py

--- src/main.py
+++ src/main.py
@@ -188,25 +188,10 @@ def main():
 	# https://networkx.github.io/documentation/stable/reference/drawing.html#module-networkx.drawing.nx_pylab
 
 	# simulate
+	print("Launching the simulation...")
 	# https://github.com/dbader/schedule
 
 	print("Finished")
 
 if __name__ == "__main__":
 	main()
-
-# TESTS ###############################################################################################################
-
-def test_main():
-	return 0
-
-def test_utilization():
-	processes = [
-		Node("0", "1", "1", "8", "0"),
-		Node("0", "1", "2", "5", "0"),
-		Node("0", "1", "2", "10", "0"),
-	]
-	assert Fraction(29, 40) == processor_use(processes)
-
-def test_sufficient_condition():
-	assert sufficient_condition(3) == 0.7797631496846196
---
+++ src/tests.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+from main import *
+
+def test_main():
+	return 0
+
+def test_utilization():
+	processes = [
+		Node("0", "1", "1", "8", "0"),
+		Node("0", "1", "2", "5", "0"),
+		Node("0", "1", "2", "10", "0"),
+	]
+	assert Fraction(29, 40) == processor_use(processes)
+
+def test_sufficient_condition():
+	assert sufficient_condition(3) == 0.7797631496846196
