89fcc08ee28d3cfa07ec985221c65c4c5ff1cf8a
Antoine <antoine.sb@orange.fr>
2019-10-08 07:59:12 +0200

    :sparkles: add coloration functions

--- src/main.py
+++ src/main.py
@@ -160,6 +160,87 @@ def is_equitable(graph: DiGraph, arch: List[int]) -> bool:
 
 	return max([graph.degree(node) for node in graph.nodes]) <= sum(arch)
 
+def generate_colorations(graph: DiGraph, arch: List[int]) -> Dict[str, Dict[object, int]]:
+	"""Generate a graph coloration with an equitable strategy if possible, or potential graph colorations with greedy strategies.
+
+	Parameters
+	----------
+	graph : DiGraph
+		The directed oriented graph imported from the *.tsk* file.
+	arch : List[int]
+		The processor architecture imported from the *.cfg* file.
+
+	Returns
+	-------
+	Dict[Dict[object, int]]
+		A dictionary containing pairs of strategy names and possible graph colorations, each coloration being a dictionary containing pairs of nodes and colors.
+	"""
+
+	if is_equitable(graph, arch):
+		return [equitable_color(graph, sum(arch))]
+	else:
+		"""Dictionary holding strategy names as keys and support for interchange as a boolean value."""
+		strategies = {
+			'largest_first': True,
+			'random_sequential': True,
+			#'smallest_last': True, # unstable for some reason
+			'independent_set': False,
+			'DSATUR': False
+		}
+
+		for strategy_name, interchange in strategies.items():
+			coloration = greedy_color(graph, strategy_name, interchange)
+			if 0 < len(coloration):
+				processes_by_core = {}
+				for key, value in sorted(greedy_color(graph, strategy_name, interchange).items()):
+					processes_by_core.setdefault(value, []).append(key)
+
+				strategies[strategy_name] = processes_by_core
+			else:
+				strategies.remove(strategy_name)
+
+		return strategies
+
+def validate_colorations(graph: DiGraph, colorations: Dict[str, Dict[object, int]]) -> Dict[str, Dict[object, int]]:
+	"""Return a list of colorations expurged from the non-schedulable colorations.
+
+	Parameters
+	----------
+	graph : DiGraph
+		The directed oriented graph imported from the *.tsk* file.
+	colorations : Dict[str, Dict[object, int]]
+		The colorations list to validate.
+
+	Returns
+	-------
+	Dict[str, Dict[object, int]]
+		A dictionary containing pairs of strings and schedulable graph colorations.
+	"""
+
+	valid_colorations = dict()
+
+	for strategy_name, coloration in colorations.items():
+		for core, process_list in coloration.items():
+			if not is_schedulable([graph.nodes(data=True)[process] for process in process_list]):
+				valid_colorations.update({strategy_name: coloration})
+
+	return valid_colorations
+
+def color_graph(graph: DiGraph, coloration: Dict[object, int]):
+	"""Color a graph using the given coloration.
+
+	Parameters
+	----------
+	graph : DiGraph
+		The directed oriented graph to color.
+	coloration : Dict[object, int]
+		The coloration to be applied to the graph.
+	"""
+
+	for node in coloration:
+		graph.nodes[node]['color'] = coloration[node]
+
+
 # ENTRY POINT #########################################################################################################
 
 def main():
