07e1c84ae6662336abd63402f68b878836b45f08
Antoine <antoine.sb@orange.fr>
2019-10-10 14:18:25 +0200

    :art: transform functions into lambdas

--- src/rate_monotonic.py
+++ src/rate_monotonic.py
@@ -2,53 +2,47 @@
 # -*- coding: utf-8 -*-
 
 from fractions import Fraction
-from typing import *
+from typing import List
 from networkx import nodes
 
-def utilization(processes: List[nodes]) -> float:
-	"""Determine the utilization load carried by a list of tasks.
+"""Determine the utilization load carried by a list of tasks.
 
-	Parameters
-	----------
-	processes : List[nodes]
-		A list of processes.
+Parameters
+----------
+processes : List[nodes]
+	A list of processes.
 
-	Returns
-	-------
-	float
-		The processor utilization, computed from the periods and WCETs of all processes.
-	"""
+Returns
+-------
+float
+	The processor utilization, computed from the periods and WCETs of all processes.
+"""
+utilization: Callable[[List[nodes]], float] = lambda processes: sum([Fraction(int(n["wcet"]), int(n["period"])) for n in processes])
 
-	return sum([Fraction(int(n["wcet"]), int(n["period"])) for n in processes])
+"""Determine the sufficient condition for schedulability of a processor or core.
 
-def sufficient_condition(count: int) -> float:
-	"""Determine the sufficient condition for schedulability of a processor or core.
+Parameters
+----------
+processes : int
+	A number of processes.
 
-	Parameters
-	----------
-	processes : int
-		A number of processes.
+Returns
+-------
+float
+	The sufficient utilization rate for a count of processes to be schedulable.
+"""
+sufficient_condition: Callable[[int], float] = lambda count: count * (pow(2, 1 / count) - 1)
 
-	Returns
-	-------
-	float
-		The sufficient utilization rate for a count of processes to be schedulable.
-	"""
+"""Determines whether a list of processes is schedulable or not.
 
-	return count * (pow(2, 1 / count) - 1)
+Parameters
+----------
+processes : List[nodes]
+	A set of periodic tasks.
 
-def is_schedulable(processes: List[nodes]) -> bool:
-	"""Determines whether a list of processes is schedulable or not.
-
-	Parameters
-	----------
-	processes : List[nodes]
-		A set of periodic tasks.
-
-	Returns
-	-------
-	bool
-		A boolean set to 'True' if the `processes` are schedulable, and 'False' otherise.
-	"""
-
-	return utilization(processes) <= sufficient_condition(len(processes))
+Returns
+-------
+bool
+	A boolean set to 'True' if the `processes` are schedulable, and 'False' otherise.
+"""
+is_schedulable: Callable[[List[nodes]], bool] = lambda processes: utilization(processes) <= sufficient_condition(len(processes))
