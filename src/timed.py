8ad117859b48c58354c7b60baa3d7592bc9b3ad0
Antoine <antoine.sb@orange.fr>
2019-10-10 21:08:02 +0200

    :sparkles: add `timed_callable` decorator

---
+++ src/timed.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# IMPORTS #############################################################################################################
+
+import logging
+from typing import Any, Callable, Optional
+from time import perf_counter
+from functools import wraps
+
+# FUNCTIONS ###########################################################################################################
+
+def timed_callable(message: str) -> Optional[Any]:
+	"""Logs the time taken by a `Callable` to run (in seconds), and returns its eventual return values.
+
+	Parameters
+	----------
+	message : str
+		The message to display before the running `Callable`.
+	callable : Callable
+		The `Callable` to run.
+	args
+		The arguments to pass to the `Callable`.
+
+	Returns
+	-------
+	result : Opional[Any]
+		The value eventually returned by the `Callable`.
+	"""
+
+	assert(0 < len(message))
+
+	@wraps(callable)
+	def callable_decorator(callable: Callable) -> Optional[Any]:
+		def timed_wrapper(*args, **kwds) -> Optional[Any]:
+
+			logging.getLogger().info('\033[92m' + message + '\033[0m')
+			start = perf_counter()
+			result = callable(*args, **kwds) # if args != None and kwargs == None
+			end = perf_counter()
+			logging.getLogger().info("\033[92mDone in " + str(end - start) + "s.\033[0m")
+
+			return result
+		return timed_wrapper
+	return callable_decorator
