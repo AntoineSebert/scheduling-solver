deb63b634208afc43e2a215df55e9d07cedcbcb9
Antoine <antoine.sb@orange.fr>
2019-10-30 16:16:12 +0100

    :sparkles: update main :pencil: documentation update

--- src/main.py
+++ src/main.py
@@ -5,9 +5,6 @@
 Resources
 - https://numpydoc.readthedocs.io/en/latest/format.html
 
-Design patterns
-- https://github.com/faif/python-patterns/blob/master/patterns/behavioral/chain_of_responsibility__py3.py
-
 Static analysis
 	tests :			https://github.com/pytest-dev/pytest
 	type checking :	https://github.com/python/mypy
@@ -24,24 +21,32 @@ import logging
 from pathlib import Path
 from argparse import ArgumentParser
 from time import process_time
-from typing import NoReturn
+from typing import NoReturn, List, Tuple
+from concurrent.futures import ThreadPoolExecutor, as_completed
+
+from tqdm import tqdm
 
 from log import colored_handler
-from solver import scheduler
-from builder import problem_builder
+from solver import solve
+from builder import build
 from formatter import OutputFormat
 
 
 # FUNCTIONS ###########################################################################################################
 
 
-def _add_dataset_arggroup(parser: ArgumentParser) -> NoReturn:
-	"""Adds a mutual exclusive group of arguments to the parser to handle dataset batch or single mode.
+def _add_dataset_arggroup(parser: ArgumentParser) -> ArgumentParser:
+	"""Adds a mutual exclusive group of arguments to the parser to handle dataset batch or single mode, then returns it.
 
 	Parameters
 	----------
 	parser : ArgumentParser
-		An `ArgumentParser` object, that will be modified.
+		An `ArgumentParser`, to which will be added an argument group.
+
+	Returns
+	-------
+	parser : ArgumentParser
+		An `ArgumentParser` holding the program's CLI.
 	"""
 
 	group = parser.add_mutually_exclusive_group(required=True)
@@ -60,6 +65,8 @@ def _add_dataset_arggroup(parser: ArgumentParser) -> NoReturn:
 		metavar='FOLDER'
 	)
 
+	return parser
+
 
 def _create_cli_parser() -> ArgumentParser:
 	"""Creates a CLI argument parser and returns it.
@@ -67,7 +74,7 @@ def _create_cli_parser() -> ArgumentParser:
 	Returns
 	-------
 	parser : ArgumentParser
-		An `ArgumentParser` object holding the program's CLI.
+		An `ArgumentParser` holding the program's CLI.
 	"""
 
 	parser = ArgumentParser(
@@ -79,7 +86,7 @@ def _create_cli_parser() -> ArgumentParser:
 	parser.add_argument(
 		'-f', '--format',
 		nargs=1,
-		default='xml',
+		default=['raw'],
 		choices=[member.name for member in OutputFormat],
 		help="Either one of " + ', '.join([member.name for member in OutputFormat]),
 		metavar="FORMAT",
@@ -93,9 +100,93 @@ def _create_cli_parser() -> ArgumentParser:
 	)
 	parser.add_argument("--version", action="version", version="%(prog)s 0.1.0")
 
-	_add_dataset_arggroup(parser)
+	return _add_dataset_arggroup(parser)
 
-	return parser
+
+def _import_files_from_folder(folder_path: Path) -> Tuple[Path, Path]:
+	"""Creates a filepath pair from a given folder.
+
+	Parameters
+	----------
+	folder_path : Path
+		A `Path` from which build the filepath pair of `*.tsk` and `*.cfg` files.
+		Only the first encountered file of each type is taken, all the others are ignored.
+
+	Returns
+	-------
+	Tuple[Path, Path]
+		A pair of `Path` pointing to the `*.tsk` and `*.cfg` files.
+	"""
+
+	tsk = next(filter(Path.is_file, folder_path.glob('*.tsk')))
+	cfg = next(filter(Path.is_file, folder_path.glob('*.cfg')))
+
+	if tsk.stem != cfg.stem:
+		logging.warning("The names of the files mismatch: '" + tsk.stem + "' and '" + cfg.stem + "'")
+
+	return (tsk, cfg)
+
+
+def _get_filepath_pairs(folder_path: Path, recursive: bool = False) -> List[Tuple[Path, Path]]:
+	"""Gathers the filepath pairs from a given folder.
+
+	Parameters
+	----------
+	folder_path : Path
+		A `Path` from which build the filepath pairs of`*.tsk` and `*.cfg` files.
+	recursive : bool
+		Toggles the recursive search for cases (default: False).
+		All the folders and subfolders containing at least one `*.tsk` and `*.cfg` file will be taken.
+
+	Returns
+	-------
+	filepath_pairs : List[Tuple[Path, Path]]
+		A list of `Tuple[Path, Path]`, the first member to a `*.tsk` file, and the second to `*.cfg` file
+	"""
+
+	filepath_pairs = list()
+
+	try:
+		filepath_pairs = [_import_files_from_folder(folder_path)]
+	except StopIteration:
+		pass
+
+	if recursive:
+		for subfolder in filter(lambda e: e.is_dir(), folder_path.iterdir()):
+			try:
+				[filepath_pairs.append(filepath) for filepath in _get_filepath_pairs(subfolder, True) if filepath]
+			except StopIteration:
+				pass
+
+	return filepath_pairs
+
+
+def _solve(filepath_pair: Tuple[str, str], format: OutputFormat, pbar: tqdm) -> str:
+	"""Handles a test case from building to solving and formatting.
+
+	Parameters
+	----------
+	filepath_pair : Tuple[str, str]
+		A pair of `Path` pointing to the `*.tsk` and `*.cfg` files.
+	format : OutputFormat
+		A member of `OutputFormat` to use to format the `Solution` of the `Problem`.
+	pbar : tqdm
+		A progress bar to update each time an action of the test case in completed.
+
+	Returns
+	-------
+	output : str
+		A `Solution` formatted as a `str` in the given format.
+	"""
+
+	problem = build(filepath_pair)
+	pbar.update()
+	solution = solve(problem)
+	pbar.update()
+	output = OutputFormat[format](solution)
+	pbar.update()
+
+	return output
 
 
 # ENTRY POINT #########################################################################################################
@@ -111,21 +202,25 @@ def main() -> int:
 	"""
 
 	args = _create_cli_parser().parse_args()
-	logging.getLogger().addHandler(
-		colored_handler(verbose=args.verbose)
-	)
+	logging.getLogger().addHandler(colored_handler(verbose=args.verbose))
 
-	problems = problem_builder(args.case, False) if args.case is not None else problem_builder(args.collection, True)
-	solutions = scheduler(problems)
+	filepath_pairs = _get_filepath_pairs(args.case, False) if args.case else _get_filepath_pairs(args.collection, True)
+
+	if not filepath_pairs:
+		raise FileNotFoundError("No matching files found. At least one *.tsk file and one *.cfg file are necessary.")
+
+	for filepath_pair in filepath_pairs:
+		logging.info("Files found:\n\t" + filepath_pair[0].name + "\n\t" + filepath_pair[1].name)
+
+	responsability_chain = frozenset([build, solve, OutputFormat])  # + printer from print
+
+	with ThreadPoolExecutor(max_workers=len(filepath_pairs)) as executor, tqdm(total=len(filepath_pairs) * 3) as pbar:
+		futures = [executor.submit(_solve, filepath_pair, args.format[0], pbar) for filepath_pair in filepath_pairs]
+		results = [future.result() for future in as_completed(futures)]
 
 	logging.info("Total ellasped time: " + str(process_time()) + "s.")
 
-	formatted = OutputFormat[args.format[0]](solutions)
-
-	for solution in formatted:
-		logging.info("Formatted solutions: \n" + solution)
-
-	return formatted # yield output ?
+	return 0
 
 
 if __name__ == "__main__":
