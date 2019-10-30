41e5ce5afdc048142f5f357d7ef8f31c579fe028
Antoine <antoine.sb@orange.fr>
2019-10-30 19:49:33 +0100

    :pencil: documentation update

--- src/formatter.py
+++ src/formatter.py
@@ -7,9 +7,9 @@
 from enum import Enum, unique
 from functools import partial
 from typing import Iterable, List
-from xml.etree.ElementTree import ElementTree, SubElement, tostring, Element
+from xml.etree.ElementTree import SubElement, tostring, Element
 from xml.dom.minidom import parseString
-import json
+from json import dumps
 
 from datatypes import Solution
 from timed import timed_callable
@@ -19,114 +19,131 @@ from timed import timed_callable
 
 
 @timed_callable("Formatting the solutions to JSON...")
-def _json_format(solutions: Iterable[Solution]) -> List[str]:
-	"""Formats an iterable of `Solution` into JSON.
+def _json_format(solution: Solution) -> str:
+	"""Formats a solution into JSON.
 
 	Parameters
 	----------
-	solutions : Iterable[Solution]
-		An iterable of `Solution`.
+	solution : Solution
+		A `Solution`.
 
 	Returns
 	-------
-	List[str]
-		A list of `str`, each representing a `Solution`.
+	str
+		A `str` representing a JSON `Solution`.
 	"""
 
-	raise NotImplementedError()
+	return dumps(solution, sort_keys=True, indent=4)
 
 
 @timed_callable("Formatting the solutions to XML...")
-def _xml_format(solutions: Iterable[Solution]) -> List[str]:
-	"""Formats an iterable of `Solution` into a custom XML schema.
+def _xml_format(solution: Solution) -> str:
+	"""Formats a solution into a custom XML schema.
 
 	Parameters
 	----------
-	solutions : Iterable[Solution]
-		An iterable of `Solution`.
+	solution : Solution
+		A `Solution`.
 
 	Returns
 	-------
-	List[str]
-		A list of `str`, each representing a `Solution`.
+	str
+		A `str` representing a XML `Solution`.
 	"""
 
-	formatted = list()
+	tables = Element("Tables")
+	for key, val in solution.items():
+		for i, tasklist in enumerate(val):
+			schedule = SubElement(tables, "Schedule", {"CpuId": str(key), "CoreId": str(i)})
+			schedule.extend(
+				[Element("Slice", {
+					"TaskId": _slice.task,
+					"Start": str(_slice.start),
+					"Duration": str(_slice.end - _slice.start)
+				}) for _slice in tasklist]
+			)
 
-	for solution in solutions:
-		tables = Element("Tables")
-		for key, val in solution.items():
-			for i, tasklist in enumerate(val):
-				schedule = SubElement(tables, "Schedule", {"CpuId": str(key), "CoreId": str(i)})
-				schedule.extend(
-					[Element("Slice", {
-						"TaskId": _slice.task,
-						"Start": str(_slice.start),
-						"Duration": str(_slice.end - _slice.start)
-					}) for _slice in tasklist]
-				)
-		formatted.append(parseString(tostring(tables)).toprettyxml())
-
-	return formatted
+	return parseString(tostring(tables)).toprettyxml()
 
 
-@timed_callable("Formatting the solutions to GRAPHML...")
-def _graphml_format(solutions: Iterable[Solution]) -> List[str]:
-	"""Formats an iterable of `Solution` into GraphML.
+@timed_callable("Formatting the solution to a raw string representation...")
+def _raw_format(solution: Solution) -> str:
+	"""Formats a solution into string.
 
 	Parameters
 	----------
-	solutions : Iterable[Solution]
-		An iterable of `Solution`.
+	solution : Solution
+		A `Solution`.
 
 	Returns
 	-------
-	List[str]
-		A list of `str`, each representing a `Solution`.
+	str
+		A `str` representing a `Solution` object.
 	"""
 
-	raise NotImplementedError()
+	return str(solution)
 
 
-@timed_callable("Formatting the solutions to JSON...")
-def _raw_format(solutions: Iterable[Solution]) -> List[str]:
-	"""Formats an iterable of `Solution` into a list of `str`.
+@timed_callable("Formatting the solution to SVG...")
+def _svg_format(solution: Solution) -> str:
+	"""Formats a solution into SVG.
 
 	Parameters
 	----------
-	solutions : Iterable[Solution]
-		An iterable of `Solution`.
+	solution : Solution
+		A `Solution`.
 
 	Returns
 	-------
-	List[str]
-		A list of `str`, each representing a `Solution`.
+	str
+		A `str` representing a SVG `Solution`.
 	"""
 
-	return [str(solution) for solution in solutions]
+	# stuff
+
+	return str(solution)
 
 
 # CLASSES #############################################################################################################
 
 @unique
 class OutputFormat(Enum):
-	"""...
+	"""An enumeratino those purpose it to map format keywords to formatting functions.
 
-	Parameters
+	Attributes
 	----------
-	solutions : Iterable[Solution]
-		...
+	xml : partial
+		Callable object mapped to a XML formatter (custom module format).
+	json : partial
+		Callable object mapped to a JSON formatter.
+	raw : partial
+		Callable object mapped to a raw formatter (`solution` is converted into a `str`).
+	svg : partial
+		Callable object mapped to the raw formatter (`solution` is converted into a `str`).
 
-	Returns
+	Methods
 	-------
-	List[str]
-		...
+	__call__
+		Converts the enumeration member into the corresponding function call.
 	"""
 
-	xml = partial(_xml_format)
-	json = partial(_json_format)
-	graphml = partial(_graphml_format)
-	raw = partial(_raw_format)
+	xml: partial = partial(_xml_format)
+	json: partial = partial(_json_format)
+	raw: partial = partial(_raw_format)
+	svg: partial = partial(_svg_format)
 
-	def __call__(self, solutions: Iterable[Solution]) -> str:
-		return self.value(solutions)
+	def __call__(self, solution: Solution) -> str:
+		"""Converts the enumeratino member into the corresponding function call.
+
+		Parameters
+		----------
+		solution : Solution
+			An `Solution`.
+
+		Returns
+		-------
+		str
+			A `str` representing a `Solution`.
+		"""
+
+		return self.value(solution)
