e1fe7b54b37a9db5ef4a42891dd114e5425a689b
Antoine <antoine.sb@orange.fr>
2019-11-06 21:35:52 +0100

    :truck: rename "formatter.py" to "format.py" :construction: WIP SVG format

--- src/format.py
+++ src/format.py
@@ -6,11 +6,13 @@
 
 from enum import Enum, unique
 from functools import partial
-from xml.etree.ElementTree import SubElement, tostring, Element
-from xml.dom.minidom import parseString
 from json import dumps
+from pathlib import Path
+from xml.dom.minidom import parseString
+from xml.etree.ElementTree import Element, SubElement, fromstringlist, tostring
 
-from datatypes import Solution, PriorityQueueEncoder
+from datatypes import PriorityQueueEncoder, Solution
+
 from timed import timed_callable
 
 
@@ -54,11 +56,11 @@ def _xml_format(solution: Solution) -> str:
 	for cpu in solution.arch:
 		for core in cpu.cores:
 			schedule = SubElement(tables, "Schedule", {"CpuId": str(cpu.id), "CoreId": str(core.id)})
-			schedule.extend(
+			schedule.extend([
 				Element("Slice", {
-					"TaskId": slice.task, "Start": str(slice.start), "Duration": str(slice.end - slice.start)
-				}) for slice in core.slices
-			)
+					"TaskId": str(_slice.task_id), "Start": str(_slice.start), "Duration": str(_slice.end - _slice.start)
+				}) for _slice in core.slices
+			])
 
 	return parseString(tostring(tables)).toprettyxml()
 
@@ -96,9 +98,53 @@ def _svg_format(solution: Solution) -> str:
 		A `str` representing a SVG `Solution`.
 	"""
 
-	# stuff
+	title = "Solution for " + (
+		str(solution.filepaths.tsk.parts[-2]) if 0 < len(solution.filepaths.tsk.parts) else str(Path.cwd())
+	)
 
-	return str(solution)
+	svg = fromstringlist([
+		"<?xml version='1.0' ?>",
+		"<svg xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' width='100%' height='100%' lang='en' version='1.1'>",
+			f"<title>{title}</title>",  # noqa: E131
+			"<desc>An horizontal chart bar showing the solution to the scheduling problem.</desc>",
+			"<style>",  # https://www.w3.org/TR/SVG2/styling.html
+			"</style>",
+			"<defs>",
+				"<symbol id='cpu' class='cpu'>",  # noqa: E131
+					"<text>CPU</text>",  # noqa: E131
+					"<g class='cores'></g>",
+				"</symbol>",
+				"<symbol id='core' class='core'>",
+					"<text>CORE</text>",
+					"<g class='slices'></g>",
+					"<path y1='0' x1='0' y2='10' x2='10' />",
+					"<marker></marker>",  # <circle cx="6" cy="6" r="3" fill="white" stroke="context-stroke" stroke-width="2"/>
+				"</symbol>",
+				"<symbol id='slice' class='slice'>",
+					"<text>SLICE</text>",
+					"<text>start</text>",
+					"<text>end</text>",
+					"<rect x='100' y='100' width='400' height='200' rx='50' fill='green' />",
+				"</symbol>",
+				"<linearGradient id='background' y2='100%'>",
+					"<stop offset='5%' stop-color='rgba(3,126,243,1)' />",
+					"<stop offset='95%' stop-color='rgba(48,195,158,1)' />",
+				"</linearGradient>",
+			"</defs>",
+			"<rect fill='url(#background)' x='0' y='0' width='100%' height='100%' />",
+			f"<text x='30%' y='10%'>{title}</text>",
+			"<g>",
+				"".join(f"<use id='cpu_{cpu.id}' xlink:href='#cpu' x='5%' y='{i * (15 * len(cpu.cores))}'>" + str(cpu.id) + "</use>" for i, cpu in enumerate(solution.arch)),
+			"</g>",
+		"</svg>"
+	])
+
+	for cpu in solution.arch:
+		Element("use", {"id": str(cpu.id)})
+		for core in cpu.cores:
+			Element("use", {"id": str(core.id)})
+
+	return tostring(svg)
 
 
 # CLASSES #############################################################################################################
